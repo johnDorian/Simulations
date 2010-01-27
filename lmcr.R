2e5ab304c73845d4b384f70219960ec1182fecb1
Jason <jlessels@gmail.com>
2010-01-27 19:13:02 +0100

    Just added more things to the code

--- lmcr.R
+++ lmcr.R
@@ -15,13 +15,18 @@
 
 ## Open the directory for the data
 
-setwd("C:/Tom/Sync/Research/Modelling/Rainfall/Data")
+setwd("~/Documents/code/Simulations/")
 
-data<-read.table("rainfall.txt", header=TRUE, sep="\t", dec=".")
-
+data<-read.csv("TP_flow.csv", header=TRUE)
+## backup the data
+back.up <- data
+## fix up the data
+data <- data[,-c(1,2)]
+names(data) <- c("X","Y","TP","FLOW")
+data$TP <- log(data$TP+1)
+data$FLOW <- log(data$FLOW+1)
 ##Non-spatial statistics
 
-hist(data$)
 
 library(gstat)
 
@@ -37,12 +42,12 @@ plot(qual)
 
 #Calculates experimental auto-semivariogram
 
-b0.vgm <- variogram(qual$b0 ~ 1, cash)
-b1.vgm <- variogram(qual$b1 ~ 1, cash)
+b0.vgm <- variogram(qual$TP ~ 1, qual)
+b1.vgm <- variogram(qual$FLOW ~ 1, qual)
 
 
 quartz()
-par(mfrow=c(2,2))
+par(mfrow=c(2,1))
 
 plot(b0.vgm[,2],b0.vgm[,3])
 plot(b1.vgm[,2],b1.vgm[,3])
@@ -54,7 +59,7 @@ b1.fit <- fit.variogram(b1.vgm,model=vgm(0,"Sph",30000,10))
 
 
 quartz()
-par(mfrow=c(2,2))
+par(mfrow=c(2,1))
 
 plot(b0.vgm,b0.fit)
 plot(b1.vgm,b1.fit)
@@ -62,12 +67,12 @@ plot(b1.vgm,b1.fit)
 ###MODELLING LINEAR MODEL OF COERGIONALISATION
 
 #Creates a dataset for auto-semivariogram estimation
-g = gstat(NULL, "b0", qual$b0 ~ 1, cash)
+g = gstat(NULL, "TP", qual$TP ~ 1, qual)
 #Somehow links b1 with b0 so it knows cross-semivariograms are needed
-g = gstat(g, "b1", qual$b1 ~ 1, cash)
+g = gstat(g, "FLOW", qual$FLOW ~ 1, qual)
 #Calculates auto and cross-semivariograms
-v = variogram(g)
-
+v = variogram(g,cutoff=200,width=200/20)
+plot(v)
 #Defines form of LMCR - range, variogram model
 g = gstat(g, model = vgm(1, "Sph", 30000, 1), fill.all = TRUE)
 #Fits LMCR model
