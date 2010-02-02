890400630cc635b36e0bbf767756f2f64d8edc5e
Jason <jlessels@gmail.com>
2010-02-02 21:23:59 +0100

    Finished the LMCR code off, added a nice little picture.

--- lmcr_cont.R
+++ lmcr_cont.R
@@ -54,10 +54,10 @@ data.bc <- data
 data.bc$TP <- bct(data$TP,TP.lambda$lambda)
 data.bc$FLOW <- bct(data$FLOW,FLOW.lambda$lambda)
 ## Create 4 geodata objects one four each variable and transformation.
-qual.tp.log<-as.geodata(data.1,coords.col=1:2,data.col=3)
-qual.flow.log<-as.geodata(data.1,coords.col=1:2,data.col=4)
-qual.tp.bc<-as.geodata(data.2,coords.col=1:2,data.col=3)
-qual.flow.bc<-as.geodata(data.2,coords.col=1:2,data.col=4)
+qual.tp.log<-as.geodata(data.log,coords.col=1:2,data.col=3)
+qual.flow.log<-as.geodata(data.log,coords.col=1:2,data.col=4)
+qual.tp.bc<-as.geodata(data.bc,coords.col=1:2,data.col=3)
+qual.flow.bc<-as.geodata(data.bc,coords.col=1:2,data.col=4)
 ## Perform likfit on each different variable
 log.tp.likfit <- likfit(qual.tp.log,ini=c(0.5,0.5),lik.method="REML")
 log.flow.likfit <- likfit(qual.flow.log,ini=c(0.5,0.5),lik.method="REML")
@@ -75,7 +75,7 @@ log.flow.psill <- summary(log.flow.likfit)$spatial.component[[2]][1]
 bc.tp.psill <- summary(bc.tp.likfit)$spatial.component[[2]][1]
 bc.flow.psill <- summary(bc.flow.likfit)$spatial.component[[2]][1]
 
-## Rip the psill from the likfits
+## Rip the nuggets from the likfits
 log.tp.nug <- summary(log.tp.likfit)$nugget.component[[2]]
 log.flow.nug <- summary(log.flow.likfit)$nugget.component[[2]]
 bc.tp.nug <- summary(bc.tp.likfit)$nugget.component[[2]]
@@ -93,10 +93,10 @@ bc.flow.range <- summary(bc.flow.likfit)$practicalRange
 log.spdf <- SpatialPointsDataFrame(data.log[,1:2],data.log)
 bc.spdf <- SpatialPointsDataFrame(data.bc[,1:2],data.bc)
 #Calculate the experimental auto-semivariogram
-log.tp.vgm <- variogram(log.spdf$TP~1,log.spdf)
-log.flow.vgm <- variogram(log.spdf$FLOW~1,log.spdf)
-bc.tp.vgm <- variogram(bc.spdf$TP~1,bc.spdf)
-bc.flow.vgm <- variogram(bc.spdf$FLOW~1,bc.spdf)
+log.tp.vgm <- variogram(log.spdf$TP~1,log.spdf,cutoff=200,width=200/20)
+log.flow.vgm <- variogram(log.spdf$FLOW~1,log.spdf,cutoff=200,width=200/20)
+bc.tp.vgm <- variogram(bc.spdf$TP~1,bc.spdf,cutoff=200,width=200/20)
+bc.flow.vgm <- variogram(bc.spdf$FLOW~1,bc.spdf,cutoff=200,width=200/20)
 
 ## 
 win.graph()
@@ -124,30 +124,72 @@ plot(bc.flow.vgm,bc.flow.fit)
 ## Completed in two sections - one for log and the second for boxcox.
 ## The first one is for logged data.
 
-#Creates a dataset for auto-semivariogram estimation
-g.log = gstat(NULL, "TP", qual.sp.1$TP ~ 1, qual.sp.1)
-#Somehow links b1 with b0 so it knows cross-semivariograms are needed
-g.log = gstat(g.log, "FLOW", qual.sp.1$FLOW ~ 1, qual.sp.1)
-#Calculates auto and cross-semivariograms
-v = variogram(g.log,cutoff=200,width=200/20)
-plot(v)
-#Defines form of LMCR - range, variogram model
+## Creates a dataset for auto-semivariogram estimation
+g.log = gstat(NULL, "TP", log.spdf$TP ~ 1, log.spdf)
+g.bc = gstat(NULL, "TP", bc.spdf$TP ~ 1, bc.spdf)
+## Somehow links b1 with b0 so it knows cross-semivariograms are needed
+g.log = gstat(g.log, "FLOW", log.spdf$FLOW ~ 1, log.spdf)
+g.bc = gstat(g.bc, "FLOW", bc.spdf$FLOW ~ 1, bc.spdf)
+## Calculates auto and cross-semivariograms
+v.log = variogram(g.log,cutoff=200,width=200/20)
+v.bc = variogram(g.bc,cutoff=200,width=200/20)
+## Calcuate the cross-semivariogram clouds for geoR.
+v.log.cloud = variogram(g.log,cutoff=200,width=200/20,cloud=TRUE)
+v.bc.cloud = variogram(g.log,cutoff=200,width=200/20,cloud=TRUE)
+plot(v.log)
+plot(v.bc)
+## Defines form of LMCR - range, variogram model
 g.log = gstat(g.log, model = vgm(1, "Sph", 3000, 1), fill.all = TRUE)
-#Fits LMCR model
-g.fit = fit.lmc(v, g.log)
-
-plot(v, g.fit)
-
-#Creates a dataset for auto-semivariogram estimation
-g.bc = gstat(NULL, "TP", qual.sp.2$TP ~ 1, qual.sp.2)
-#Somehow links b1 with b0 so it knows cross-semivariograms are needed
-g.bc = gstat(g.log, "FLOW", qual.sp.2$FLOW ~ 1, qual.sp.2)
-#Calculates auto and cross-semivariograms
-v = variogram(g.bc,cutoff=200,width=200/20)
-plot(v)
-#Defines form of LMCR - range, variogram model
 g.bc = gstat(g.bc, model = vgm(1, "Sph", 3000, 1), fill.all = TRUE)
-#Fits LMCR model
-g.fit = fit.lmc(v, g.bc)
+## Subset the variograms just for TP.FLOW values
+v.log.cloud<-subset(v.log.cloud,v.log.cloud$id=="TP.FLOW")
+v.bc.cloud<-subset(v.bc.cloud,v.bc.cloud$id=="TP.FLOW")
+## Create dummy variograms in the geoR package.
+dummy.log<-variog(qual.tp.log,estimator.type="classical",max.dist=200,option=c("cloud"))
+dummy.bc<-variog(qual.tp.bc,estimator.type="classical",max.dist=200,option=c("cloud"))
+## Replace the dummy values with that of the cross semivariograms
+dummy.log$u<-v.log.cloud$dist
+dummy.log$v<-v.log.cloud$gamma
 
-plot(v, g.fit)
+dummy.bc$u<-v.bc.cloud$dist
+dummy.bc$v<-v.bc.cloud$gamma
+
+## Use variofit in the geoR package to fit the semivariogram models
+log<-variofit(dummy.log,ini=c(0.5,0.5),cov.model="sph", fix.nugget=F,max.dist=200)
+bc<-variofit(dummy.bc,ini=c(0.5,0.5),cov.model="sph", fix.nugget=F,max.dist=200)
+## Now uing the values fitted in the geoR model set the vgm() in the gstat() function.
+g.log = gstat(g.log, model = vgm(summary(log)$spatial.component[[1]][1], "Sph", log$practicalRange, log$nugget), fill.all = TRUE)
+
+g.bc = gstat(g.bc, model = vgm(summary(bc)$spatial.component[[1]][1], "Sph", bc$practicalRange, bc$nugget), fill.all = TRUE)
+
+## Fits the LMCR models
+g.log.fit = fit.lmc(v.log, g.log)
+g.bc.fit = fit.lmc(v.bc, g.bc)
+## Now plot them to have a look at the result
+plot(v.log, g.log.fit, main="log transformed LMCR")
+win.graph()
+plot(v.bc, g.bc.fit,main="Boxcox transformed LMCR")
+g.log.fit
+#data:
+#TP : formula = log.spdf$TP`~`1 ; data dim = 461 x 4
+#FLOW : formula = log.spdf$FLOW`~`1 ; data dim = 461 x 4
+#variograms:
+#           model     psill    range
+#TP[1]        Nug 0.2508796  0.00000
+#TP[2]        Sph 0.4938174 49.65972
+#FLOW[1]      Nug 0.5856350  0.00000
+#FLOW[2]      Sph 3.5357701 49.65972
+#TP.FLOW[1]   Nug 0.1957239  0.00000
+#TP.FLOW[2]   Sph 0.9019855 49.65972
+g.bc.fit
+#data:
+#TP : formula = bc.spdf$TP`~`1 ; data dim = 461 x 4
+#FLOW : formula = bc.spdf$FLOW`~`1 ; data dim = 461 x 4
+#variograms:
+#           model        psill    range
+#TP[1]        Nug 1.718051e-04  0.00000
+#TP[2]        Sph 7.714721e-04 49.65972
+#FLOW[1]      Nug 4.172702e+02  0.00000
+#FLOW[2]      Sph 1.461639e+03 49.65972
+#TP.FLOW[1]   Nug 1.199014e-01  0.00000
+#TP.FLOW[2]   Sph 7.642805e-01 49.65972
