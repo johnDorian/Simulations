1e499f575602b1f47da3d09d21a105a1f9dd416b
Jason <jlessels@gmail.com>
2010-02-23 11:46:51 +0100

    Organised the repo. lmcr.R is the main script again

--- lmcr.R
+++ lmcr.R
@@ -1,88 +1,93 @@
 #!/usr/bin/Rscript
 ################################################################################
 ################################################################################
-### Title: LMCR.R ##############################################################
+### Title: LMCR_FINAL.R ########################################################
 ### Author: Jason Lessels j.lessels@usyd.edu.au ################################
-### Created: Tue Jan 26 16:39:42 2010 ##########################################
-### Modified: Tue Jan 26 16:39:42 2010 #########################################
-### Aim: To fit a one dimensional LMCR #########################################
-### Description: This will use the gstat package to fit a LMCR #################
+### Created: Wed Feb  3 15:42:52 2010 ##########################################
+### Modified: Wed Feb  3 15:42:52 2010 #########################################
+### Aim: To perform LMCR #######################################################
+### Description: This script performs LMCR on temporal data ####################
 ################################################################################
-### NOTES: This will use ML not REML ###########################################
+### NOTES: This still isn't great but it's the best there is. A further note is
+### in the original script (lmcr_cont.R) likfit was used to determine the vario-
+### gram variables, but this was found not to make any difference in the end re-
+### sults. Therefore variofit is still for the cross-variogram only. ###########
 ################################################################################
-### TODO: Transfer the script from it's source to be compable with this code. ##
+### TODO: Run through all the possiblities #####################################
 ################################################################################
 
-## Open the directory for the data
+###GET SOME USEFUL FUNCTIONS OUT OF THE WAY.
+## Let's universalise plotting on all Oprating Systems.
+win.graph = function(){if(.Platform$OS=='unix') x11() else win.graph=win.graph}
+## Now so we can easly check the determinates of fit.lmc using det.fit
+det.fit <- function(object){
+tpn <- object$model$TP$psill[1]
+fln <- object$model$FLOW$psill[1]
+tpfln <- object$model$TP.FLOW$psill[1]
+d <- matrix(c(tpn,tpfln,tpfln,fln),c(2,2))
+print(det(d))
+}
 
+###Now we can actually start with the script.
+
+## Set the working directory.
 setwd("~/Documents/code/Simulations/")
-
+## Load the datasets.
 data<-read.csv("TP_flow.csv", header=TRUE)
-## backup the data
+## Load the nessacry libraries. A couple for variograms, and some for the boxcox transformations.
+library(gstat);library(geoR);library(MASS);library(TeachingDemos);library(moments)
+## Clean up the dataset - create a back.up of the data
 back.up <- data
-## fix up the data
 data <- data[,-c(1,2)]
 names(data) <- c("X","Y","TP","FLOW")
-data$TP <- log(data$TP+1)
-data$FLOW <- log(data$FLOW+1)
-##Non-spatial statistics
+## Add arbituary value to the data, so we can transform the data.
+data$TP <- data$TP+0.01
+data$FLOW <- data$FLOW+0.01
+## Determine the correct lambda values for the transformations of both TP and Flow
+TP.lambda <- boxcox.fit(data$TP)
+FLOW.lambda <- boxcox.fit(data$FLOW)
+data <- data
+data$TP <- bct(data$TP,TP.lambda$lambda)
+data$FLOW <- bct(data$FLOW,FLOW.lambda$lambda)
 
+##Create gstat objects of the data
+spdf <- SpatialPointsDataFrame(data[,1:2],data)
 
-library(gstat)
+### STEP 1. Create A gstat object with TP and flow.
+g = gstat(NULL, "TP", bc.spdf$TP ~ 1, bc.spdf,,maxdist=200)
+g = gstat(g, "FLOW", bc.spdf$FLOW ~ 1, bc.spdf,maxdist=200)
 
-##Creates spatial points class
+### STEP 2. Create intial values for the models.
+#Create variogarms for plotting.
+v = variogram(g.bc,cutoff=200,width=200/20)
+#Plot the cross covariogram and get some initial values for it.
+plot(v,g)
+#The cross covariance models using initial values, these will change.
+g = gstat(g,id=c("TP","FLOW"),model=vgm(0.5,"Sph",48,0.2),fill.all=TRUE)
 
-qual<-SpatialPointsDataFrame(data[,1:2],data)
+### STEP 3. Find the range of each covariogram using fit.lmc
+##Use fit.lmc() function to find the ranges for each covariogram.
+fit.range=fit.lmc(v,g,fit.lmc=TRUE,fit.ranges=TRUE)
+fit.range
+##Avergae the ranges out and use the average range as the max range.
+range<-mean(fit.range$model$TP[[3]][2],fit.range$model$TP.FLOW[[3]][2],fit.range$model$FLOW[[3]][2])
+g.ave.range = gstat(g,id=c("TP","FLOW"),model=vgm(0.5,"Sph",range,0.2),fill.all=TRUE)
+##Create another model using 2 days (48) as the range).
+g.2days.range=gstat(g,id=c("TP","FLOW"),model=vgm(0.5,"Sph",48,0.2),fill.all=TRUE)
 
-summary(qual)
-
-plot(qual)
-
-###MODELLING AUTO-SEMIVARIOGRAMS
-
-#Calculates experimental auto-semivariogram
-
-b0.vgm <- variogram(qual$TP ~ 1, qual)
-b1.vgm <- variogram(qual$FLOW ~ 1, qual)
-
-
-quartz()
-par(mfrow=c(2,1))
-
-plot(b0.vgm[,2],b0.vgm[,3])
-plot(b1.vgm[,2],b1.vgm[,3])
-
-#Fits auto-semivariograms
-
-b0.fit <- fit.variogram(b0.vgm,model=vgm(0,"Sph",30000,10))
-b1.fit <- fit.variogram(b1.vgm,model=vgm(0,"Sph",30000,10))
-
-
-quartz()
-par(mfrow=c(2,1))
-
-plot(b0.vgm,b0.fit)
-plot(b1.vgm,b1.fit)
-
-###MODELLING LINEAR MODEL OF COERGIONALISATION
-
-#Creates a dataset for auto-semivariogram estimation
-g = gstat(NULL, "TP", qual$TP ~ 1, qual)
-#Somehow links b1 with b0 so it knows cross-semivariograms are needed
-g = gstat(g, "FLOW", qual$FLOW ~ 1, qual)
-#Calculates auto and cross-semivariograms
-v = variogram(g,cutoff=200,width=200/20)
-plot(v)
-#Defines form of LMCR - range, variogram model
-g = gstat(g, model = vgm(1, "Sph", 30000, 1), fill.all = TRUE)
-#Fits LMCR model
-g.fit = fit.lmc(v, g)
-
-plot(v, g.fit)
-
-
-
-
-
-
-
+### STEP 4. Fit the two final models using fit.lmc()
+fit.ave=fit.lmc(v,g.ave.range,fit.lmc=TRUE,fit.ranges=FALSE)
+fit.2days=fit.lmc(v,g.2days.range,fit.lmc=TRUE,fit.ranges=FALSE)
+##Check the determinates of the model make sure they are positive
+det.fit(fit.ave)
+det.fit(fit.2days)
+##Plotting the results.
+pdf("lmc_with_ave_range.pdf")
+plot(v,fit.ave,main="Average range used for model")
+dev.off()
+win.graph()
+pdf("lmc_with_2day_range.pdf")
+plot(v,fit.2days,main="2 day range used for model")
+##Compare the values for the four best models.
+fit.ave
+fit.2days
