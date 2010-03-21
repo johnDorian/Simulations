c6088fc371c8a16a994a85449dc9a4678e6e6f3c
Jason <jlessels@gmail.com>
2010-03-22 10:55:20 +1100

    Finally have the lmcr script working

--- lmcr_convert.R
+++ lmcr_convert.R
@@ -28,18 +28,6 @@ setwd("~/Documents/code/Simulations")
 ###dm - holds maximum change for each parameter (isomorphic to c).
 ###DATAF is (nvar,nvar, 3 [h,gamma,npairs,st. dev of gamma at h], nlag [lines]).
 
-###To get everything working
-	guessa=ranger(fit)
-	nlags=matrix(c(length(semvar[,1]),length(semvar[,1]),length(semvar[,1])))
-	lock=1
-	cpar=55000 #must be below 100,000
-	modtyp=4 #that is iso.exp
-	icvp=1
-	wgt=1
-	maxdist=200
-	semvar<-semVar(v)
-	covar<-covStructure(fit)
-test<-lmcr(semvar,nolags,wgt,icvp,cpar,modtyp,covar,maxdist,guessa,lock)
 
 lmcr<-function(semvar,nolags,wgt,icvp,cpar,modtyp,covar,maxdist,guessa,lock){
 ### obtain samvar from lmcr.R script.
@@ -639,3 +627,53 @@ lag[1]<-object$model[[1]]$range[2]
 lag[2]<-object$model[[3]]$range[2]
 return(lag)
 }
+
+gammah<-function(h,modtyp,co,c1,a1,c2,a2){
+	alpha=1.75
+	sill=co+c1+c2
+
+	switch(modtyp,
+		iso.lin=return(co*h),
+		iso.cir=
+		if(h>a1)
+			return(sill)
+		else{
+			hovera=h/a1
+			angle=atan(sqrt(1-hovera^2)/(hovera+0.000001))
+			h4=0.63662*hovera*sqrt(1-hovera^2)
+			return(co+c1*(0.63662*angle+h4))
+		},
+		
+		iso.sph=
+		if(h>a1)
+			return(sill)
+		else{
+			hovera=h/a1
+			return(co+c1*(1.5*hovera-0.5*hovera^3))
+		},
+		iso.exp=return(co+c1*(1-exp(-h/a1))),
+		
+		iso.pen=if(h>a1)
+			return(sill)
+		else
+			return(co+c1*(1.875*(h/a1)-1.25*(h/a1)^3+0.375*(h/a1)^5)),
+		
+		iso.dou=if(h>a2)
+			return(sill)
+		else
+			if(h>a1)
+				return(co+c1+c2*(1.5*(h/a2)-0.5*(h/a2)^3))
+			else
+				return(co+c1*(1.5*(h/a1)-0.5*(h/a1)^3)+c2*(1.5*(h/a2)-0.5*(h/a2)^3)),
+
+		dou.exp=return(co+c1(1-exp((-h/a1)))+c2*(1-exp((-h/a2)))),
+		
+		stable=return(co+c1*(1-exp(-h^alpha/a1^alpha))),
+		
+		dou.sta=return(co+c1*(1-exp(-h^alpha/a1^alpha))+c2*(1-exp(-h/a2))),
+		
+		bro=return(co*h^a1)
+	)
+}
+		
+		
